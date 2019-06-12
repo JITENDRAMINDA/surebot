f3691a0566ffb06a51082d9ffa51f3ef2821c622
Suresh133 <51027983+Suresh133@users.noreply.github.com>
2019-06-12 11:51:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,9 +13,36 @@ s = '-1001274887387'
 
 def forawrd(client, message):
 
-   client.send_message(int(u),"**" + message.text + "**")
-           
+    mes = client.send_message(int(u), "**" + message.text + "**")
+    file = open("sure.txt" , "r")
+    lines = file.readlines()
+    file.close()
+    for line in lines:
+         files = open("sure.txt" , "w")
+         files.write( line + " " + str(message.message_id) +  " " + str(mes.message_id))
+         files.close()       
 
+
+
+
+@app.on_message(Filters.chat(int(s)) & Filters.sticker)
+
+def forawrd(client, message):
+
+   client.send_sticker(int(u),message.sticker.file_id)
+            
+@app.on_message(Filters.chat(int(s)) & Filters.text & Filters.edited)
+
+def forawrd(client, message):
+
+  file = open("sure.txt" , "r")
+  lines = file.readlines()
+  file.close()
+  for line in lines:
+    x = line.split()
+    id = str(message.message_id)
+    if id in x:
+      client.edit_message_text(int(u),int(x[x.index(id)+1]), "**" + message.text + "**" )
 
 
 

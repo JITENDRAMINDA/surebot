a7a8946b5b3a862986be7793cb9271714d5bfa8a
Suresh133 <51027983+Suresh133@users.noreply.github.com>
2019-06-21 11:45:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,22 @@ def forawrd(client, message):
  if message.from_user.id == 700761174:
   message.reply("Sorry Bhai, Nhi sikha sakta")
  else:
-  message.reply("Don't Spam, wait Surendra reply you soon.")
- 
+ file = open("userlist.txt" , "r")
+ lines = file.readlines()
+ file.close()
+ for line in lines:
+  t = line.split()
+  for m in t:
+   if not m == message.from_user.id:
+     message.reply("Don't Spam, wait Surendra reply you soon.")
+     fil = open("userlist.txt" , "r")
+     li = fil.readlines()
+     fil.close()
+     for lin in li:
+      x = lin.split()
+      for y in x:
+       fil = open("userlist.txt" , "w")
+       li = fil.write(y + message.from_user.id)
+       fil.close()
+       
 app.run()

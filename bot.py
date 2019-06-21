03701d5daee86fb941f3491210db335b40ee30e3
Suresh133 <51027983+Suresh133@users.noreply.github.com>
2019-06-21 11:21:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,63 +1,15 @@
 from pyrogram import Client, Filters
 
 
-app = Client('765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc',605563,"7f2c2d12880400b88764b9b304e14e0b")
+app = Client('my_accccount',605563,"7f2c2d12880400b88764b9b304e14e0b")
 
 s = '-1001274887387'
 
-@app.on_message(Filters.chat(int(s)) & Filters.text & ~ Filters.edited)
+@app.on_message(Filters.private)
 def forawrd(client, message):
- fil = open("sue.txt" , "r")
- c = fil.readlines()
- fil.close()
- for e in c:
-  mes = client.send_message(int(e), "**" + message.text + "**")
-  file = open("sure.txt" , "r")
-  lines = file.readlines()
-  file.close()
-  for line in lines:
-   files = open("sure.txt" , "w")
-   files.write( line + " " + str(message.message_id) +  " " + str(mes.message_id))
-   files.close()       
-
-@app.on_message(Filters.chat(int(s)) & Filters.text & Filters.edited)
-def forawrd(client, message):
- fil = open("sue.txt" , "r")
- c = fil.readlines()
- fil.close()
- for e in c:
-  file = open("sure.txt" , "r")
-  lines = file.readlines()
-  file.close()
-  for line in lines:
-   x = line.split()
-   id = str(message.message_id)
-   if id in x:
-    client.edit_message_text(int(e),int(x[x.index(id)+1]), "**" + message.text + "**" )
-@app.on_message(Filters.command("set"))
-def forawrd(client, message):
- if message.from_user.id == 491634139 :
-  file = open("sue.txt" , "w")
-  file.write(message.text.split(' ')[1])
-  file.close()
-  message.reply('done')
- elif message.from_user.id == 837065534 :
-  file = open("sue.txt" , "w")
-  file.write(message.text.split(' ')[1])
-  file.close()
-  message.reply('done')
-
-@app.on_message(Filters.command("clear"))
-def forawrd(client, message):
- if message.from_user.id == 491634139 :
-  file = open("sure.txt" , "w")
-  file.write("001")
-  file.close()
-  message.reply('done')
- elif message.from_user.id == 837065534 :
-  file = open("sure.txt" , "w")
-  file.write("001")
-  file.close()
-  message.reply('done')
-
+ if message.from_user.id == 700761174:
+  message.reply("Sorry Bhai, Nhi sikha sakta")
+ else:
+  message.reply("Don't Spam, wait Surendra reply you soon.")
+ 
 app.run()

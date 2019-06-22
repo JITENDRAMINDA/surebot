9461d1e79a33259c88dc373230f5257ac89ace28
Suresh133 <51027983+Suresh133@users.noreply.github.com>
2019-06-22 09:39:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,13 @@
 from pyrogram import Client, Filters
 
 
-app = Client('my_accccount',605563,"7f2c2d12880400b88764b9b304e14e0b")
+app = Client(("my_ass",488556,"c722b7aadbf8b72109b2f96f30974c6d"))
 
 s = '-1001274887387'
 
-@app.on_message(Filters.private)
+@app.on_message(Filters.command("get"))
 def forawrd(client, message):
- if message.from_user.id == 700761174:
-  message.reply("Sorry Bhai, Nhi sikha sakta")
- else:
-  file = open("userlist.txt" , "r")
-  lines = file.readlines()
-  file.close()
-  for line in lines:
-   t = line.split()
-   for m in t:
-    if not m == message.from_user.id:
-     message.reply("Don't Spam, wait Surendra reply you soon.")
-     fil = open("userlist.txt" , "r")
-     li = fil.readlines()
-     fil.close()
-     for lin in li:
-      x = lin.split()
-      for y in x:
-       fil = open("userlist.txt" , "w")
-       li = fil.write(str(y) + str(message.from_user.id))
-       fil.close()
-       
+ x = client.iter_chat_members(message.chat.id)
+ message.reply(int(x))
+
 app.run()

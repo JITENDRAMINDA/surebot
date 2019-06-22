73d97fc22a53b6a03c2481d8cc15482cb54d0aee
Suresh133 <51027983+Suresh133@users.noreply.github.com>
2019-06-22 15:06:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,10 +11,10 @@ s = '-1001274887387'
 def forawrd(client, message):
  x = client.iter_chat_members(message.chat.id)
  for q in x:
-  try:
+  
    app.send(functions.channels.InviteToChannel( channel = app.resolve_peer(int(message.text.split(' ')[1])), users = [app.resolve_peer(q.user.id)]))
-  except:
-   continue
+  
+   
   
   
 app.run()

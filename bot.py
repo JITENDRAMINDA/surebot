1f39f55bf0af8e4641851665679c071713e83aed
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-09-23 21:58:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,11 +3,11 @@ from pyrogram.api import functions, types
 app = Client("my_acc",854941,"bf9632f82af99dc8c3b934ab48d54780")
 @app.on_message(Filters.command("get"))
 def forawrd(client, message):
- x = client.iter_chat_members(message.chat.id)
+ x = client.iter_chat_members(message.text.split(" ")[1])
  for q in x:
   try:
-   app.send(functions.channels.InviteToChannel( channel = app.resolve_peer(int(message.text.split(' ')[1])), users = [app.resolve_peer(q.user.id)]))
+   app.send(functions.channels.InviteToChannel( channel = app.resolve_peer(int(message.text.split(' ')[2])), users = [app.resolve_peer(q.user.id)]))
   except:
-   client.send_message(-1001356076506,str(q.user.id))
+   client.send_message(-1001397779415,str(q.user.id))
    continue 
 app.run()
